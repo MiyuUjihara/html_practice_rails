8a1ac6a1055f74a16a3847ecd27b37002acc8c6c
MiyuUjihara <miuji08@gmail.com>
2022-09-29 18:21:41 +0900

    カレーの宿題

---
+++ app/controllers/soup_curry_controller.rb
@@ -0,0 +1,4 @@
+class SoupCurryController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get "top",  to: "top#index"
   get "traning",  to: "traning#index"
   get "adidas",  to: "adidas#index"
-
+  get "soup_curry",  to: "soup_curry#index"
 end
